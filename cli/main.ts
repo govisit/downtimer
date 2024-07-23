144b1f7268bd02db66c8fa7647573cbd7464e1cc
Mario Bašić <mario@laravelista.hr>
2024-07-23 12:38:00 +0200

    feat(cli): bumps version number

--- cli/main.ts
+++ cli/main.ts
@@ -5,7 +5,7 @@ import { command as timerCommand } from "./commands/timers/index.ts";
 
 const main = new Command()
   .name("DownTimer")
-  .version("0.2.0")
+  .version("0.3.1")
   .description("When your phone or PC timer is not enough.")
   .action(() => {
     main.showHelp();
